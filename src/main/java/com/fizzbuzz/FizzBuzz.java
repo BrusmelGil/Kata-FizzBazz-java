e981565ca542f5a9781b6d68c73a9d7223b8fcc8
Brusmel Gil <Brusmel@outlook.es>
2023-12-07 12:21:38 +0100

    not complete

--- src/main/java/com/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/fizzbuzz/FizzBuzz.java
@@ -1,5 +1,27 @@
 package com.fizzbuzz;
 
 public class FizzBuzz {
+
+
+    public String replaceNumber(int numb) {
+
+        boolean isDivisibleBy3 = numb % 3 == 0;
+        boolean isDivisibleBy5 = numb % 5 == 0;
+
+
+        if (isDivisibleBy3) return "Fizz";
     
+        if (isDivisibleBy5) return "Buzz";
+
+        if (isDivisibleBy3 && isDivisibleBy5) return "FizzBuzz";
+
+
+        return null;
+
+        
+    }
+
+
+    
+
 }
--- src/test/java/com/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/com/fizzbuzz/FizzBuzzTest.java
@@ -1,5 +1,6 @@
 package com.fizzbuzz;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -7,8 +8,38 @@ import org.junit.jupiter.api.Test;
 public class FizzBuzzTest {
 
     @Test
-    public void test_fizzbuzz_example() {
-        assertTrue(true);
+    public void test_fizzbuzzreplaceNumber_canReturnFizz() {
+        //give
+        int numb = 3;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        //when
+
+        String word = fizzBuzz.replaceNumber(numb);
+
+        //then
+
+    assertEquals("Fizz", word);
     }
 
+    @Test
+    public void test_fizzbuzzreplaceNumber_canReturnBuzz() {
+        //give
+        int numb = 5;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        //when
+
+        String word = fizzBuzz.replaceNumber(numb);
+
+        //then
+
+    assertEquals("Buzz", word);
+    }
+
+
+
+
 }
+
+
